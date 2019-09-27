ead9a929a197d52c012c9d538ccf084a0db5f627
Dee Barizo <deebarizo@gmail.com>
2019-09-26 23:26:01 -0500

    Add styled components

--- src/components/PictureContainer.js
+++ src/components/PictureContainer.js
@@ -1,22 +1,60 @@
 import React from "react";
+import styled from "styled-components";
+
+const PictureDiv = styled.div`
+  border-top: 1px solid black;
+`;
+
+const ParagraphDiv = styled.div`
+  width: 830px;
+  margin: 2.5% auto;
+  text-align: left;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    margin-left: 6%;
+    margin-right: 6%;
+  }
+`;
+
+const StyledP = styled.p``;
+
+const LoadingDiv = styled.div`
+  margin-top: 20%;
+`;
+
+const ImgDiv = styled.div`
+  margin: 0 auto;
+  padding: 0 5%;
+  max-width: 830px;
+`;
+
+const ResponsiveImg = styled.img`
+  min-width: 100%;
+  width: 100%;
+`;
 
 export default function PictureContainer(props) {
   const { title, date, url, explanation } = props;
 
   if (!title) {
     return (
-      <div className="picture">
-        <h2>Loading...</h2>
-      </div>
+      <PictureDiv>
+        <LoadingDiv>
+          <h2>Loading...</h2>
+        </LoadingDiv>
+      </PictureDiv>
     );
   }
 
   return (
-    <div className="picture">
+    <PictureDiv>
       <h2>{title}</h2>
       <h3>{date}</h3>
-      <img src={url} alt={title} />
-      <p>{explanation}</p>
-    </div>
+      <ImgDiv>
+        <ResponsiveImg src={url} alt={title} />
+      </ImgDiv>
+      <ParagraphDiv>{explanation}</ParagraphDiv>
+    </PictureDiv>
   );
 }
